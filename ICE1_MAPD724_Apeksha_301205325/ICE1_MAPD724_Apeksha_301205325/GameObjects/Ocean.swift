03ce7f6a22390cfdc6192fccdee14af9ebf85aee
Apeksha Parmar <appiparmar879916@gmail.com>
2023-01-22 16:50:34 -0500

    ICE#1

---
+++ ICE1_MAPD724_Apeksha_301205325/ICE1_MAPD724_Apeksha_301205325/GameObjects/Ocean.swift
@@ -0,0 +1,54 @@
+//
+//  Ocean.swift
+//  ICE1_MAPD724_Apeksha_301205325
+//
+//  Created by Apeksha Parmar on 2023-01-19.
+//
+
+import GameplayKit
+import SpriteKit
+
+class Ocean : GameObject
+{
+    // constructor / initializer
+    init()
+    {
+        super.init(imageString: "ocean", initialScale: 2.0)
+        Start()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func Start()
+    {
+        zPosition = 0
+        verticalSpeed = 5.0
+    }
+    
+    override func Update()
+    {
+        Move()
+        CheckBounds()
+    }
+    
+    override func CheckBounds()
+    {
+        if(position.y <= -2107)
+        {
+            Reset()
+        }
+    }
+    
+    override func Reset()
+    {
+        position.y = 2107
+    }
+    
+    func Move()
+    {
+        position.y -= verticalSpeed!
+    }
+}
+
---
+++ ICE1_MAPD724_Apeksha_301205325/ICE1_MAPD724_Apeksha_301205325/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  ICE1_MAPD724_Apeksha_301205325
+//
+//  Created by Apeksha Parmar on 2023-01-19.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        if let scene = GKScene(fileNamed: "GameScene")
+        {
+            if let sceneNode = scene.rootNode as! GameScene?
+            {
+                sceneNode.scaleMode = .aspectFill
+                if let view = self.view as! SKView?
+                {
+                    view.presentScene(sceneNode)
+                    view.ignoresSiblingOrder = true
+                }
+            }
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+    {
+        return .portrait
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
